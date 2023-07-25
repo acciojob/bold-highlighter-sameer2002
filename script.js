a35c98bf287b6adfc8cb895d3a285661870f38ab
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-25 14:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+  var bold = document.querySelectorAll('strong')
+  bold.forEach(function(word) {
+    word.style.color = 'green';
+  });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ var bold = document.querySelectorAll('strong')
+  bold.forEach(function(word) {
+    word.style.color = 'black';
+  });
     
 }
